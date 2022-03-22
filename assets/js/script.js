efbd46b8a4d2b952d27afdbccdcc67bae532f46a
Erika Cannon <cannone.rva@gmail.com>
2022-03-22 13:43:39 -0400

    edit functions on tasks and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,8 +125,52 @@ $(".list-group").on("click", "p", function() {
 
   });
 
+  // due date was clicked
+  $(".list-group").on("click", "span", function() {
+    var date = $(this)
+    .text()
+    .trim();
 
+    //create new input element
+    var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
 
+    //swap out elements
+    $(this).replaceWith(dateInput);
+
+    // automatically focus on new element
+    dateInput.trigger("focus");
+  });
+
+  //value of due date was changed
+  $(".list-group").on("blur", "input[type='text']", function(){
+    //get current text
+    var date = $(this)
+    .val()
+    .trim();
+    
+    var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+    var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+    tasks[status][index].date = date;
+    saveTasks();
+
+    //recreate span element with bootstrap classes
+    var taskSpan =$("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+    //replace input with span element
+    $(this).replaceWith(taskSpan);
+  });
 
 
 
